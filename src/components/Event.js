44250a79ebe3107b457df78a4de64b35710a00d1
Alex Bignell <alex@bignell.me>
2016-02-18 16:53:16 -0800

    Add scaffold for Event and EventList components

---
+++ src/components/Event.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// takes in {name, venue, date, time}
+
+class Event extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Event</div>;
+  }
+}
+
+export default Event;
