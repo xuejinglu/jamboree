2e02abbd1a1d7dba6d134db198730b035f7a527b
Alex Bignell <alex@bignell.me>
2016-02-18 17:22:52 -0800

    Refactor searchEventful as ES6 module

--- src/lib/searchEventful.js
+++ src/lib/searchEventful.js
@@ -1,25 +1,30 @@
-var options = {
-  app_key: 'jh8HDfBxzXZC7sZ5',
-  location: 94102,
-  category: 'music',
-  page_size: 20,
-  date: 'Today'
-};
+import { eventful } from '../config/apiKeys';
+import $ from 'jquery';
 
-var searchEventful = (options, callback) => {
+const searchEventful = (callback) => {
+  const options = {
+    app_key: eventful,
+    location: 94102,
+    category: 'music',
+    page_size: 20,
+    date: 'Today',
+  };
+
   $.ajax({
-    url: "http://api.eventful.com/json/events/search",
+    url: 'http://api.eventful.com/json/events/search',
     type: 'GET',
     data: options,
     contentType: 'application/json',
-    success: function (data) {
+    success: (data) => {
       console.log(data);
       callback(data);
     },
-    error: function (data) {
+    error: (data) => {
       console.error('Eventful AJAX failed to GET');
       console.log('problem is ', JSON.parse(data.responseText));
       callback(data);
-    }
-});
+    },
+  });
 };
+
+export default searchEventful;
