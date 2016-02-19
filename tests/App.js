3dd1a3bdcf4942ae276dc7764fba70b7b71135fc
Boya Jiang <jiang.boya@yahoo.com>
2016-02-18 16:50:23 -0800

    Merge changes with main

--- tests/App.js
+++ tests/App.js
@@ -4,21 +4,7 @@ import { render } from 'enzyme';
 import { App } from '../src/App';
 
 describe('Basic App test', () => {
-<<<<<<< HEAD
-  it('shallow render', () => {
-    const result = shallow(<App />);
-    expect(result.contains(<h1>Hello World!</h1>)).to.equal(true);
-  });
-
-  it('full render', () => {
-    const result = mount(<App />);
-    expect(result.find('h1').length).to.equal(1);
-  });
-
-  it('static render', () => {
-=======
   it('rendered the map component', () => {
->>>>>>> 1c18208714b11654144d2ed9f8379d453059ce0e
     const result = render(<App />);
     expect(result.text()).to.contain('Map');
   });
