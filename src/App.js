289b925135fc67a3d7b0948981969f899ca4706f
Chris Bassano <christo4b@gmail.com>
2016-02-18 19:42:38 -0800

    added map and app rendering functionality and initMap helper function. created empty CSS file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Map from './components/Map';
+import initMap from '../../public/Helpers'
+import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
 
 export class App extends Component {
-  render() {
-    return <Map />;
+  constructor(props){
+    super(props);
+    this.state = {
+      mapOptions: {
+        center: {lat: 37.7833, lng: 122.4167},
+        zoom: 3
+      },
+      map: null
+    }
   }
+
+
+  render () {
+    return (
+      <Map />
+    );
+  }
+
 }
 
-ReactDOM.render(<App/>, document.querySelector('#app'));
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+  // render() {
+  //   console.log("this.state.mapOptions", this.state.mapOptions);
+  //   return (
+  //     <div>
+  //       <div>
+
+  //       </div>
+  //       <div>
+  //         <Map />
+  //       </div>
+  //     </div>
+  //     )
+  // }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
+import initMap from '../../public/Helpers'
 
 class Map extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  initMap() {
+    var myLatLng = {lat: -25.363, lng: 131.044};
+
+    console.log("WHAT!!!");
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: myLatLng
+    });
+
+    console.log(map);
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
+
+
+  }
+
   render() {
-    return <div>Map</div>;
+    console.log("we're in Map render!!");
+
+    return (
+      <div> {this.initMap()}</div>
+      )
   }
 }
 
 export default Map;
+
+
+
+
