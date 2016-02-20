892c8c8c5bcd5fbce06b285b9ed21470a8834f69
Alex Bignell <alex@bignell.me>
2016-02-19 20:18:21 -0800

    Add tests and some changes to Event and EventList components

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,34 +1,23 @@
 import React, { Component } from 'react';
 
-const SAMPLE_DATA = {
-  name: 'Parliament Funkadelic',
-  date: '2016-12-17T10:00:00',
-  venue: {
-    name: 'The Fillmore',
-    lat: 37.7840042,
-    lng: -122.43313319999999,
-  },
-  description: 'Give up the funk.',
-  thumbnail: 'http://path.to.eventful.cdn.com/thumbnail.jpg',
-};
-
 class Event extends Component {
   constructor(props) {
     super(props);
-    // this.state = { data: props.data };
-    this.state = { data: SAMPLE_DATA };
+    this.state = { event: props.data };
   }
 
   render() {
     return (
       <article>
-        <h4>{ this.state.data.name }</h4>
-        <h5>{ this.state.data.venue.name }</h5>
-        <h6>{ this.state.data.date }</h6>
-        <p>{ this.state.data.description }</p>
+        <h4>{ this.state.event.name }</h4>
+        <h5>{ this.state.event.venue.name }</h5>
+        <h6>{ this.state.event.date }</h6>
+        <p>{ this.state.event.description }</p>
       </article>
     );
   }
 }
+
+Event.propTypes = { data: React.PropTypes.object.isRequired };
 
 export default Event;
--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,47 +1,10 @@
 import React, { Component } from 'react';
-import Event from 'event';
-
-const SAMPLE_DATA = [
-  {
-    name: 'The Beegees',
-    date: '2016-12-15T08:00:00',
-    venue: {
-      name: 'Civic Auditorium',
-      lat: 37.7780405,
-      lng: -122.41741339999999,
-    },
-    description: 'Stayin\' alive.',
-    thumbnail: 'http://path.to.eventful.cdn.com/thumbnail.jpg',
-  },
-  {
-    name: 'Parliament Funkadelic',
-    date: '2016-12-17T10:00:00',
-    venue: {
-      name: 'The Fillmore',
-      lat: 37.7840042,
-      lng: -122.43313319999999,
-    },
-    description: 'Give up the funk.',
-    thumbnail: 'http://path.to.eventful.cdn.com/thumbnail.jpg',
-  },
-  {
-    name: 'Pink Floyd',
-    date: '2016-12-17T08:00:00',
-    venue: {
-      name: 'The Warfield',
-      lat: 37.7826598,
-      lng: -122.41018099999997,
-    },
-    description: 'Just another brick in the wall.',
-    thumbnail: 'http://path.to.eventful.cdn.com/thumbnail.jpg',
-  },
-];
+import Event from './event';
 
 class EventList extends Component {
   constructor(props) {
     super(props);
-    // this.state = { events: props.events };
-    this.state = { events: SAMPLE_DATA };
+    this.state = { events: props.data };
   }
 
   render() {
@@ -54,5 +17,7 @@ class EventList extends Component {
     );
   }
 }
+
+EventList.propTypes = { data: React.PropTypes.object.isRequired };
 
 export default EventList;
