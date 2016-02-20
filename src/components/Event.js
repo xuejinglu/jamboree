b4b3abea74330ca2c13c3e9722bd7b56d523aed8
Alex Bignell <alex@bignell.me>
2016-02-19 19:00:08 -0800

    Implement Event and EventList components

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 
-// takes in {name, venue, date, time}
+const SAMPLE_DATA = {
+  name: 'Parliament Funkadelic',
+  date: '2016-12-17T10:00:00',
+  venue: {
+    name: 'The Fillmore',
+    lat: 37.7840042,
+    lng: -122.43313319999999,
+  },
+  description: 'Give up the funk.',
+  thumbnail: 'http://path.to.eventful.cdn.com/thumbnail.jpg',
+};
 
 class Event extends Component {
   constructor(props) {
     super(props);
+    // this.state = { data: props.data };
+    this.state = { data: SAMPLE_DATA };
   }
 
   render() {
-    return <div>Event</div>;
+    return (
+      <article>
+        <h4>{ this.state.data.name }</h4>
+        <h5>{ this.state.data.venue.name }</h5>
+        <h6>{ this.state.data.date }</h6>
+        <p>{ this.state.data.description }</p>
+      </article>
+    );
   }
 }
 
--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
 import Event from 'event';
 
-// takes in [{event},{event},{event}]
+const SAMPLE_DATA = [
+  {
+    name: 'The Beegees',
+    date: '2016-12-15T08:00:00',
+    venue: {
+      name: 'Civic Auditorium',
+      lat: 37.7780405,
+      lng: -122.41741339999999,
+    },
+    description: 'Stayin\' alive.',
+    thumbnail: 'http://path.to.eventful.cdn.com/thumbnail.jpg',
+  },
+  {
+    name: 'Parliament Funkadelic',
+    date: '2016-12-17T10:00:00',
+    venue: {
+      name: 'The Fillmore',
+      lat: 37.7840042,
+      lng: -122.43313319999999,
+    },
+    description: 'Give up the funk.',
+    thumbnail: 'http://path.to.eventful.cdn.com/thumbnail.jpg',
+  },
+  {
+    name: 'Pink Floyd',
+    date: '2016-12-17T08:00:00',
+    venue: {
+      name: 'The Warfield',
+      lat: 37.7826598,
+      lng: -122.41018099999997,
+    },
+    description: 'Just another brick in the wall.',
+    thumbnail: 'http://path.to.eventful.cdn.com/thumbnail.jpg',
+  },
+];
 
 class EventList extends Component {
   constructor(props) {
     super(props);
+    // this.state = { events: props.events };
+    this.state = { events: SAMPLE_DATA };
   }
 
   render() {
     return (
       <section className="eventList">
-        <Event />
-        <Event />
-        <Event />
+        {this.state.events.map((event, i) =>
+          <Event key={i} data={event} />
+        )}
       </section>
     );
   }
