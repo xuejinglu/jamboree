18b71f56ff2c4e2e1fc2772f8f7a7dda95dcd927
Boya Jiang <jiang.boya@yahoo.com>
2016-02-26 21:53:53 -0800

    Modulate out the banner component and add an image and jumplink

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+var Banner = () => (
+  <div id="banner-container">
+    <div className="banner">
+      <p>See what events are happening around you. Check out what's happening at local venues - find your Jamboree. </p>
+      <a href="#mainApp" className="jumpButton btn btn-primary">Get Started</a>
+    </div>
+  </div>
+);
+
+export default Banner;
