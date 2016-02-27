3f9758465b1e77c0549182e32e68b787aa3eede7
Chris Bassano <christo4b@gmail.com>
2016-02-27 15:30:28 -0800

    eventModel schema to have location, dateRange, and array of events. eventController getList function queries database with location and date from request params object

--- server/events/eventController.js
+++ server/events/eventController.js
@@ -3,24 +3,26 @@ var Q = require('q');
 var http = require('http');
 var request = require('request');
 
-module.exports = {
-  getEventsFromDb: function(req, res, next) {
+var findEvent = Q.nbind(Event.findOne, Event);
 
-    find {location: "string", date: "YYYYMMDD00-YYYYMMDD00"}
+module.exports = {
+  getEvents: function(req, res, next) {
+    findEvent({
+      location: req.params.location,
+      date: req.params.date,
+    })
+    .then(function(doc){
+      if (doc) {
+        console.log("DOC ------> ", doc);
+        res.json(doc);
+      } else {
+        eventfulRequest(); //TODO invoke API call
+      }
+    });
   },
 
+  addEvents: function(req, res, next) {
 
-
-
-
+  }
 
 };
-
-
-Location: string (zip or city)
-Date:
-
-
-Event Model
-
-location | dateRange | categories | eventObj {title: lat: long: category: }
--- server/events/eventModel.js
+++ server/events/eventModel.js
@@ -1,49 +1,18 @@
 var mongoose = require('mongoose');
 
 var EventSchema = new mongoose.Schema({
-  city_name: {
+  location: {
     type: String,
     required: true,
   },
-  country_name: {
+  date: {
     type: String,
     required: true,
   },
-  latitude: {
-    type: String,
-    required: true,
-  },
-  longitude: {
-    type: String,
-    required: true,
-  },
-  postal_code: {
-    type: String,
-    required: true,
-  },
-  start_time: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  url: {
-    type: String,
-    required: true,
-  },
-  venue_address: {
-    type: String,
-    required: true,
-  },
-  venue_name: {
-    type: String,
-    required: true,
-  },
-  venue_url: {
-    type: String,
-    required: true,
+  eventList: {
+    type: Array,
   },
 });
+
+
 
