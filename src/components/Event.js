6ac5aaa73f78e6d339b182af386bd24e3a037af4
Chris Bassano <christo4b@gmail.com>
2016-02-29 12:09:47 -0800

    fixed linter issue with event display

--- src/components/Event.js
+++ src/components/Event.js
@@ -7,20 +7,14 @@ class Event extends Component {
   }
 
   render() {
-    console.log(this.props.data);
     return (
-      <tr>
-        <td> <img src={this.props.data.image.thumb.url}></img> </td>
-        <td>
         <article>
           <h4>{ this.props.data.title }</h4>
           <div>
-            <img src='http://maps.google.com/mapfiles/ms/icons/blue-dot.png'></img>
+            <img src="http://maps.google.com/mapfiles/ms/icons/blue-dot.png"></img>
             <h5>{ this.props.data.venue_name }</h5>
           </div>
         </article>
-        </td>
-
     );
   }
 }
