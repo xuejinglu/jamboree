07bd13df5b722e65f648fe2aa99c0458ff205d6a
Boya Jiang <jiang.boya@yahoo.com>
2016-02-29 17:16:48 -0800

    Remove extraneous unused code from App.js

--- src/App.js
+++ src/App.js
@@ -72,44 +72,6 @@ export class App extends Component {
     });
   }
 
-  componentDidMount() {
-
-
-    // var input = document.getElementById('locationField');
-    //  var infowindow = new google.maps.InfoWindow();
-    // var options = {
-    //   types: ['cities']
-    // };
-    // const autocomplete = new google.maps.places.Autocomplete(input, options);
-    // autocomplete.addListener('place_changed', function() {
-    //   var place = autocomplete.getPlace();
-    //   if (!place.geometry) {
-    //     window.alert("Autocomplete's returned place contains no geometry");
-    //     return;
-    //   }
-
-    //   // If the place has a geometry, then present it on a map.
-    //   if (place.geometry.viewport) {
-    //     map.fitBounds(place.geometry.viewport);
-    //   } else {
-    //     map.setCenter(place.geometry.location);
-    //     map.setZoom(17);  // Why 17? Because it looks good.
-    //   }
-
-    //   var address = '';
-    //   if (place.address_components) {
-    //     address = [
-    //       (place.address_components[0] && place.address_components[0].short_name || ''),
-    //       (place.address_components[1] && place.address_components[1].short_name || ''),
-    //       (place.address_components[2] && place.address_components[2].short_name || '')
-    //     ].join(' ');
-    //   }
-
-    //   infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    //   infowindow.open(map, marker);
-    // });
-  }
-
   /*eslint-disable */
   render() {
     return (
