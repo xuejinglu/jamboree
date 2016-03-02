d22650578c996cbc82e109360be9453622e4256f
Jake Shasteen <jakeshasteen@gmail.com>
2016-03-01 17:35:02 -0800

    Add search youtube function

---
+++ src/utils/youtube.js
@@ -0,0 +1,18 @@
+export default function searchYouTube ( options, callback ) => {
+  const URL = 'https://www.googleapis.com/youtube/v3/search';
+  $.ajax( {
+    url: URL,
+    type: 'GET',
+    data: { 
+      part: 'snippet',
+      type: 'video', 
+      videoEmbeddable: true, 
+      q: options.query, 
+      maxResults: options.max, 
+      key: options.key
+    },
+    contentType: 'application/json',
+    success: callback,
+    error: ( error ) => { console.error( 'Error: Could not complete API request.', error.responseText ); }
+  } );
+};
