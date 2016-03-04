2e0b09f9408772e7abae7e26189102a00d598110
Jake Shasteen <jakeshasteen@gmail.com>
2016-03-03 19:34:59 -0800

    Display event date prettily

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,16 +3,21 @@ import React, { Component } from 'react';
 class Event extends Component {
   constructor(props) {
     super(props);
+    this.start_date = new Date( this.props.data.start_time );
+    this.date_str = this.start_date.toString()
   }
 
   render() {
     return (
         <article>
           <h4 onClick={ this.props.clickHandler.bind(null, this.props.data) }>{ this.props.data.title }</h4>
-          <div>
-            <img src="http://maps.google.com/mapfiles/ms/icons/pink-dot.png"></img>
-            <h5>{ this.props.data.venue_name }</h5>
-          </div>
+            <div className="event">
+              <img className="pin" src="http://maps.google.com/mapfiles/ms/icons/pink-dot.png"></img>
+              <div className="eventText">
+                <span className="venueName">{ this.props.data.venue_name }</span>
+                <div className="eventDate">{ this.date_str.substr(0, 21) } { this.date_str.substr(-5) }</div>
+              </div>
+            </div>
         </article>
     );
   }
--- src/components/EventList.js
+++ src/components/EventList.js
@@ -16,10 +16,11 @@ class EventList extends Component {
       <div className="col-xs-4 eventList">
       <VideoPlayer video={ this.props.video } />
         <section>
-          <div className="events-header"></div>
+          <div className="events">
           {this.props.data.map((event, i) =>
             <Event key={i} data={event} clickHandler={ this.clickHandler.bind(this) } />
           )}
+          </div>
         </section>
       </div>
     );
