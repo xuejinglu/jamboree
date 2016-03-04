0aae3046599be753b8c154ee0598a5717c0f2554
Jing Lu <jing.xuejing.lu@gmail.com>
2016-03-03 20:09:45 -0800

    implement map link

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,10 +8,15 @@ class Map extends Component {
     /*eslint-disable */
     this.mapStyle = [{'featureType':'administrative.neighborhood','elementType':'labels.text','stylers':[{'visibility':'simplified'}]},{'featureType':'all','elementType':'labels.text.fill','stylers':[{'color':'#ffffff'}]},{'featureType':'all','elementType':'labels.text.stroke','stylers':[{'color':'#000000'},{'lightness':13}]},{'featureType':'administrative','elementType':'geometry.fill','stylers':[{'visibility':'off'},{'color':'#000000'}]},{'featureType':'administrative','elementType':'geometry.stroke','stylers':[{'color':'#144b53'},{'weight':1.4},{'lightness':14}]},{'featureType':'landscape','elementType':'all','stylers':[{'color':'#08304b'}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'visibility':'on'},{'color':'#000000'}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'visibility':'on'},{'color':'#0b434f'},{'lightness':25}]},{'featureType':'road.arterial','elementType':'geometry.fill','stylers':[{'color':'#000000'}]},{'featureType':'road.arterial','elementType':'geometry.stroke','stylers':[{'color':'#0b3d51'},{'lightness':16}]},{'featureType':'road.local','elementType':'geometry','stylers':[{'color':'#000000'}]},{'featureType':'transit','elementType':'all','stylers':[{'visibility':'off'},{'color':'#146474'}]},{'featureType':'water','elementType':'all','stylers':[{'color':'#021019'}]},{'featureType':'poi','elementType':'all','stylers':[{'visibility':'off'}]},{'featureType':'road.highway','elementType':'labels.icon','stylers':[{'visibility':'off'}]},{'featureType':'road.arterial','elementType':'labels.text','stylers':[{'visibility':'off'}]},{'featureType':'road.local','elementType':'labels','stylers':[{'visibility':'off'}]},{'featureType':'administrative.land_parcel','elementType':'all','stylers':[{'visibility':'on'}]},{'featureType':'administrative.locality','elementType':'all','stylers':[{'visibility':'off'}]},{'featureType':'road','elementType':'labels.icon','stylers':[{'visibility':'off'}]}];
     /*eslint-enable */
+    this.state = {
+      map: {},//this.getMap(this.getLatLng(this.props)),
+    }
   }
 
-  componentDidMount() {
-    this.renderMap();
+  componentDidMount(){
+    this.setState({
+      map: this.getMap(this.getLatLng(this.props)),
+    })
   }
 
   getLatLng(props) { //eslint-disable-line
@@ -29,20 +34,15 @@ class Map extends Component {
     });
   }
 
-  shouldComponentUpdate(nextProps) {
+  componentDidUpdate(nextProps) {
+    let map = this.state.map;
     const myLatLng = this.getLatLng(nextProps);
-    const map = this.getMap(myLatLng);
     this.renderPins(nextProps.parentState.events, map);
     if (myLatLng.lat && myLatLng.lng) {
       map.setCenter(myLatLng);
       map.setZoom(14);
     }
     return true;
-  }
-
-  renderMap() {
-    const myLatLng = this.getLatLng(this.props);
-    this.getMap(myLatLng);
   }
 
   renderPins(events, map) {
