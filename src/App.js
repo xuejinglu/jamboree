62c84c1478af468a10ab73e64cf34cadfe62549c
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-03-05 13:55:18 -0800

    edited keys for deployment

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Search from './components/Search';
 import EventList from './components/EventList';
 import Banner from './components/Banner';
 import getYouTube from './utils/youtube.js';
-import keys from './config/apikeys.js';
-import Login from './components/Login';
 import $ from 'jquery';
 import getloc from './utils/getloc';
 import getdate from './utils/getdate';
